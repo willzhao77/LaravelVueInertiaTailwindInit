d7931addfdd5f653664d101482b9fa927a1da05d
Will Zhao <will@publica.co.nz>
2022-09-29 23:07:34 +1300

    add unplugin-vue-components.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import Components from "unplugin-vue-components/vite";
 
 export default defineConfig({
     plugins: [
@@ -15,6 +16,13 @@ export default defineConfig({
                     includeAbsolute: false,
                 },
             },
+        }),
+        Components({
+            resolvers: [],
+            dirs: ["resources/js/Components"],
+            dts: true,
+            include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+            directoryAsNamespace: true,
         }),
     ],
 });
